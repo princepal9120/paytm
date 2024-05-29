22584d93274402c350ba20b2b50ad1f43481920d
Prince Pal <princepal9120@gmail.com>
2024-05-29 11:31:24 +0530

    add backend routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
+// backend/index.js
 const express = require("express");
+const cors =require("cors")
+app.use(cors())
+app.use(express.json())
+const rootRouter = require("./routes/index");
 
+const app = express();
 
+app.use("/api/v1", rootRouter); 
+app.listen(3000)
